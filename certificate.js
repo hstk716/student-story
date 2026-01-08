e9fc2fb81fc35487d40b2a95457cf1a39de289af
hstk716 <bvmglobalprn67@gmail.com>
2026-01-08 11:19:27 +0530

    Add files via upload

--- certificate.js
+++ certificate.js
@@ -1,20 +1,32 @@
 export async function generateCertificate() {
   const data = JSON.parse(localStorage.getItem("student_data"));
-  if (!data || !data.studentName) return alert("Fill form first!");
+  if (!data || !data.studentName) {
+    alert("Fill form first!");
+    return;
+  }
 
   const { jsPDF } = window.jspdf;
-  const doc = new jsPDF({ orientation: "landscape", unit: "px", format: [800, 600] });
+  const doc = new jsPDF({
+    orientation: "landscape",
+    unit: "px",
+    format: [800, 600],
+  });
+
   const img = new Image();
-  img.src = "certificate.png"; 
+  img.crossOrigin = "anonymous";
 
   img.onload = function () {
-    doc.addImage(img, "PNG", 0, 0, 800, 600);
+    doc.addImage(img, "JPEG", 0, 0, 800, 600);
     doc.setFont("helvetica", "bold");
     doc.setTextColor(50, 50, 50);
     doc.setFontSize(40);
     doc.text(data.studentName, 400, 390, { align: "center" });
     doc.save(`${data.studentName}_Certificate.pdf`);
   };
-  img.onerror = () => alert("Error: certificate.png not found!");
 
+  img.onerror = function () {
+    alert("Error: certificate.jpeg not found!");
+  };
+
+  img.src = "certificate.jpeg";
 }
