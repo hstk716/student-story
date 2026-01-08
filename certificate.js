0412e9dd93ec5c37769f138948a7fbc5a897b884
hstk716 <bvmglobalprn67@gmail.com>
2026-01-08 11:52:26 +0530

    Add files via upload

--- certificate.js
+++ certificate.js
@@ -26,9 +26,9 @@ export async function generateCertificate() {
 
     // FORCE string rendering
     const name = String(data.studentName).trim();
-    doc.text(name, 400, 350, {
+    doc.text(name, 400, 420, {
   align: "center",
-  baseline: "top"
+  baseline: "alphabetic"
 });
 
 
